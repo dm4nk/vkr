cc69193d8633643b72897635a199a7eb3bd8f13c
dm4nk <dim.xx2011@yandex.ru>
2023-04-03 00:38:15 +0400

    small changes in correlation

--- correlation.py
+++ correlation.py
@@ -4,13 +4,10 @@ import seaborn as sns
 
 
 def run():
-    df = pd.read_csv("data/1_try/dataset.csv")
-    df = df.drop(df[df.domain != 'rentvchannel'].index)
-    domains = df.domain.unique()
-    domains_dict = dict(zip(domains, range(len(domains))))
-    df['domain_id'] = df.domain.map(domains_dict)
-    plt.figure(figsize=(21, 21))
+    df = pd.read_csv("data/dataset_united_extended.csv")
+    plt.figure(figsize=(30, 30))
     sns.heatmap(df.corr(), center=0, annot=True, fmt='.1%', cmap="PiYG")
+    plt.yticks(rotation=0)
     plt.show()
 
 
